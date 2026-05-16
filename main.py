960432dec38a2bc7f2848ff9e090ae46a8961113
adityakarwani11 <adityakarwani44@gmail.com>
2026-05-16 14:09:22 +0530

    Updated

--- main.py
+++ main.py
@@ -128,5 +128,10 @@ def categories():
 
 # Start the server
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    mcp.run(
+        transport="http",
+        host="0.0.0.0",
+        port=port
+    )
     # mcp.run()
